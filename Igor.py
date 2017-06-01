477f344349f22d505865c83cf2412d1dc1c67af4
Ígor Yamamoto <igor.a.r.y@gmail.com>
2017-06-01 10:10:35 -0300

    Read purchase events

--- Igor.py
+++ Igor.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[44]:
 
 
 import pandas as pd
@@ -9,7 +9,10 @@ import numpy as np
 import json
 import gzip
 import seaborn as sns
+from pandas.io.json import json_normalize
 
+
+# ## Open Catalog dataset
 
 # In[2]:
 
@@ -23,25 +26,57 @@ catalog = pd.read_csv('data/catalog.gz')
 catalog.head()
 
 
-# In[66]:
+# ## Open dataset with a JSON object per line with NaN
+
+# In[4]:
 
 
 get_ipython().run_cell_magic(u'time', u'', u"with gzip.open('data/test.gz', 'r') as f:\n    json_records = []\n    for line in f:\n        record = json.loads(line)\n        json_records.append(record)\ndata = pd.DataFrame(json_records)")
 
 
-# In[67]:
+# In[5]:
 
 
 data.head()
 
 
-# In[98]:
+# ## Open only purchase events from data dataset
+
+# In[6]:
 
 
 get_ipython().run_cell_magic(u'time', u'', u"with open('data/purchase_data', 'r') as f:\n    json_records = []\n    for line in f:\n        record = json.loads(line)\n        json_records.append(record)\npurchase = pd.DataFrame(json_records)")
 
 
-# In[99]:
+# In[7]:
+
+
+purchase.head()
+
+
+# ## Unnest products
+
+# In[42]:
+
+
+get_ipython().run_cell_magic(u'time', u'', u"with open('data/purchase_data', 'r') as f:\n    json_records = []\n    for line in f:\n        record = json.loads(line)\n        products = record.pop('products')\n        record.pop('source')\n        record.pop('event_type')\n        for obj in products:\n            new_record = record.copy()\n            new_record.update(obj)\n            json_records.append(new_record)\npurchase = pd.DataFrame(json_records)")
+
+
+# In[43]:
+
+
+purchase.head()
+
+
+# ### Using json_normalize
+
+# In[48]:
+
+
+get_ipython().run_cell_magic(u'time', u'', u"with open('data/purchase_data', 'r') as f:\n    json_records = []\n    for line in f:\n        record = json.loads(line)\n        json_records.append(record)\npurchase = json_normalize(json_records, 'products', ['uid','date','gender'])")
+
+
+# In[49]:
 
 
 purchase.head()
