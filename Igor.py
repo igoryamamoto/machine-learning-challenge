f31a328ad36174a81df87ea8ad227e5d6c0b61e6
Ígor Yamamoto <igor.a.r.y@gmail.com>
2017-06-01 17:41:50 -0300

    Add more models

--- Igor.py
+++ Igor.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import pandas as pd
@@ -17,13 +17,13 @@ get_ipython().magic(u'matplotlib inline')
 
 # ## Open Catalog dataset
 
-# In[3]:
+# In[2]:
 
 
 catalog = pd.read_csv('data/catalog.gz')
 
 
-# In[107]:
+# In[3]:
 
 
 catalog.head()
@@ -31,13 +31,13 @@ catalog.head()
 
 # ## Open dataset with a JSON object per line with NaN
 
-# In[108]:
+# In[4]:
 
 
 get_ipython().run_cell_magic(u'time', u'', u"with gzip.open('data/test.gz', 'r') as f:\n    json_records = []\n    for line in f:\n        record = json.loads(line)\n        json_records.append(record)\ndata = pd.DataFrame(json_records)")
 
 
-# In[109]:
+# In[5]:
 
 
 data.head()
@@ -45,13 +45,13 @@ data.head()
 
 # ## Open only purchase events from data dataset
 
-# In[110]:
+# In[6]:
 
 
 get_ipython().run_cell_magic(u'time', u'', u"with open('data/purchase_data', 'r') as f:\n    json_records = []\n    for line in f:\n        record = json.loads(line)\n        json_records.append(record)\npurchase = pd.DataFrame(json_records)")
 
 
-# In[111]:
+# In[7]:
 
 
 purchase.head()
@@ -59,13 +59,13 @@ purchase.head()
 
 # ## Unnest products
 
-# In[128]:
+# In[8]:
 
 
 get_ipython().run_cell_magic(u'time', u'', u"with open('data/purchase_data', 'r') as f:\n    json_records = []\n    for line in f:\n        record = json.loads(line)\n        products = record.pop('products')\n        record.pop('source')\n        record.pop('event_type')\n        for obj in products:\n            new_record = record.copy()\n            new_record.update(obj)\n            json_records.append(new_record)\npurchase = pd.DataFrame(json_records)")
 
 
-# In[113]:
+# In[9]:
 
 
 purchase.head()
@@ -73,31 +73,31 @@ purchase.head()
 
 # ### Using json_normalize
 
-# In[4]:
+# In[10]:
 
 
 get_ipython().run_cell_magic(u'time', u'', u"with open('data/purchase_data', 'r') as f:\n    json_records = []\n    for line in f:\n        record = json.loads(line)\n        json_records.append(record)\npurchase = json_normalize(json_records, 'products', ['uid','date','gender'])")
 
 
-# In[115]:
+# In[11]:
 
 
 purchase.head()
 
 
-# In[5]:
+# In[12]:
 
 
 purchase = purchase.join(catalog.set_index('pid'), on='pid')
 
 
-# In[117]:
+# In[13]:
 
 
 purchase.info()
 
 
-# In[6]:
+# In[14]:
 
 
 categorical = ['category', 'sub_category', 'sub_sub_category','gender']
@@ -105,13 +105,13 @@ new_purchase = pd.get_dummies(purchase, columns=categorical)
 new_purchase.info()
 
 
-# In[119]:
+# In[15]:
 
 
 new_purchase.head()
 
 
-# In[7]:
+# In[16]:
 
 
 dummy_cols = ['category_c1bd5fd999bd577743936898ada96496b547af3c',
@@ -125,7 +125,7 @@ purchase2.info()
 # ---
 # # Machine Learning Algorithms
 
-# In[8]:
+# In[40]:
 
 
 from sklearn.naive_bayes import GaussianNB
@@ -134,11 +134,15 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn import linear_model
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import LinearSVC
+from sklearn.calibration import calibration_curve
 
 
 # ## Separate train and test data
 
-# In[9]:
+# In[18]:
 
 
 features = purchase2.columns[6:-1]
@@ -147,32 +151,32 @@ target = purchase2.columns[-1]
 
 # ## Gambiarra : nem todas as features tão no target
 
-# In[10]:
+# In[19]:
 
 
 get_ipython().run_cell_magic(u'time', u'', u"with open('data/purchase_target', 'r') as f:\n    json_records = []\n    for line in f:\n        record = json.loads(line)\n        json_records.append(record)\npurchase3 = json_normalize(json_records, 'products', ['uid','date'])\npurchase3 = purchase3.join(catalog.set_index('pid'), on='pid')\ncategorical = ['category', 'sub_category', 'sub_sub_category']\nnew_purchase2 = pd.get_dummies(purchase3, columns=categorical)\ndummy_cols = ['category_c1bd5fd999bd577743936898ada96496b547af3c',\n'sub_category_f08770a96fb546673053ab799f5ea9cada06c06a',\n'sub_sub_category_2d2c44a2d8f18a6271f0e8057313af68a46d0f24']\npurchase4 = new_purchase2.drop(dummy_cols, 1)\nfeatures2 = purchase4.columns[6:]")
 
 
-# In[11]:
+# In[20]:
 
 
 new_features = list(set(features)&set(features2))
 
 
-# In[13]:
+# In[21]:
 
 
 X = purchase2[new_features]
 Y = purchase2[target]
 
 
-# In[14]:
+# In[22]:
 
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.4, random_state=42)
 
 
-# In[15]:
+# In[23]:
 
 
 len(x_train.columns)
@@ -180,13 +184,13 @@ len(x_train.columns)
 
 # ## Naive Bayes
 
-# In[16]:
+# In[24]:
 
 
 get_ipython().run_cell_magic(u'time', u'', u'gnb = GaussianNB()\ngnb.fit(x_train, y_train)\ny_pred = gnb.predict(x_test)\nscore = metrics.accuracy_score(y_test, y_pred)')
 
 
-# In[17]:
+# In[25]:
 
 
 score
@@ -208,13 +212,13 @@ score
 
 # ## Decision Tree
 
-# In[21]:
+# In[26]:
 
 
 get_ipython().run_cell_magic(u'time', u'', u'tree = DecisionTreeClassifier()\ntree.fit(x_train, y_train)\ny_pred = tree.predict(x_test)\nscore = metrics.accuracy_score(y_test, y_pred)')
 
 
-# In[22]:
+# In[27]:
 
 
 score
@@ -222,13 +226,72 @@ score
 
 # ## Forest Tree
 
-# In[23]:
+# In[43]:
 
 
-get_ipython().run_cell_magic(u'time', u'', u'forest = RandomForestClassifier()\nforest.fit(x_train, y_train)\ny_pred = tree.predict(x_test)\nscore = metrics.accuracy_score(y_test, y_pred)')
+get_ipython().run_cell_magic(u'time', u'', u'forest = RandomForestClassifier(n_estimators=100)\nforest.fit(x_train, y_train)\ny_pred = tree.predict(x_test)\nscore = metrics.accuracy_score(y_test, y_pred)')
 
 
-# In[24]:
+# In[44]:
+
+
+score
+
+
+# ## xgboost
+
+# In[35]:
+
+
+get_ipython().run_cell_magic(u'time', u'', u'boost = GradientBoostingClassifier()\nboost.fit(x_train, y_train)\ny_pred = boost.predict(x_test)\nscore = metrics.accuracy_score(y_test, y_pred)')
+
+
+# In[36]:
+
+
+score
+
+
+# ## SGDClassifier
+
+# In[31]:
+
+
+clf = linear_model.SGDClassifier()
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+score = metrics.accuracy_score(y_test, y_pred)
+
+
+# In[32]:
+
+
+score
+
+
+# ## Logistic Regression
+
+# In[38]:
+
+
+get_ipython().run_cell_magic(u'time', u'', u'clf = linear_model.LogisticRegression(C=1e5)\nclf.fit(x_train, y_train)\ny_pred = clf.predict(x_test)\nscore = metrics.accuracy_score(y_test, y_pred)')
+
+
+# In[39]:
+
+
+score
+
+
+# ## SVM
+
+# In[49]:
+
+
+get_ipython().run_cell_magic(u'time', u'', u'svc = LinearSVC(C=1.0)\nsvc.fit(x_train, y_train)\ny_pred = svc.predict(x_test)\nscore = metrics.accuracy_score(y_test, y_pred)')
+
+
+# In[50]:
 
 
 score
