757d0e9684588987710510a13b42e594a0014b64
Ígor Yamamoto <igor.a.r.y@gmail.com>
2017-06-09 19:32:07 -0300

    Fix huge mistake

--- Igor_final.py
+++ Igor_final.py
@@ -19,7 +19,7 @@ from sklearn.externals import joblib
 get_ipython().magic(u'matplotlib inline')
 
 
-# In[42]:
+# In[11]:
 
 
 def open_json(data_file):    
@@ -42,7 +42,7 @@ def to_weekday(timestamp):
     except ValueError:
         weekday = datetime.strptime(timestamp, "%Y-%m-%d").weekday()
     except TypeError:
-        weekday = 9
+        weekday = -1
     return weekday
 
 def convert_date(timestamp):
@@ -51,7 +51,7 @@ def convert_date(timestamp):
     except ValueError:
         res = datetime.strptime(timestamp, "%Y-%m-%d")
     except TypeError:
-        res = 9
+        res = -1
     return res
 
 def get_data_pageview(page_type):
@@ -74,57 +74,96 @@ def get_target_pageview(page_type):
 
 # ## Create 5 DataFrames: catalog, purchase, pageview, target_purchase, target_pageview
 
-# In[43]:
+# In[16]:
 
 
-get_ipython().run_cell_magic(u'time', u'', u"catalog = pd.read_csv('data/catalog', usecols=['pid', 'current_price','original_price', 'category', 'sub_category', 'sub_sub_category'])\ncatalog = catalog.fillna(value=0)\ncatalog = catalog[catalog.current_price!=0]\ncatalog['original_price'] = np.where(catalog['original_price'] == 0, catalog['current_price'], catalog['original_price'])\ncatalog['diff_price'] = catalog['original_price'] - catalog['current_price']\n\npurchase_data = open_json('data/sub_data/purchase')           \npurchase = json_normalize(purchase_data, 'products', ['date','gender','uid'])\n\npageview_data = open_json('data/sub_data/products')   \npageview = pd.DataFrame(pageview_data, columns=['productId', 'timestamp', 'gender', 'uid'])\npageview.columns = ['pid', 'date', 'gender', 'uid']\n\n\npurchase_data = open_json('data/sub_target/purchase')           \ntarget_purchase = json_normalize(purchase_data, 'products', ['date','uid'])\n\npageview_data = open_json('data/sub_target/purchase')   \ntarget_pageview = pd.DataFrame(pageview_data, columns=['productId', 'timestamp', 'uid'])\ntarget_pageview.columns = ['pid', 'date', 'uid']")
+get_ipython().run_cell_magic(u'time', u'', u"catalog = pd.read_csv('data/catalog', usecols=['pid', 'current_price','original_price', 'category', 'sub_category', 'sub_sub_category'])\ncatalog = catalog.fillna(value=0)\ncatalog = catalog[catalog.current_price!=0]\ncatalog['original_price'] = np.where(catalog['original_price'] == 0, catalog['current_price'], catalog['original_price'])\ncatalog['diff_price'] = catalog['original_price'] - catalog['current_price']\n\npurchase_data = open_json('data/sub_data/purchase')           \npurchase = json_normalize(purchase_data, 'products', ['date','gender','uid'])\n\npageview_data = open_json('data/sub_data/products')   \npageview = pd.DataFrame(pageview_data, columns=['productId', 'timestamp', 'gender', 'uid'])\npageview.columns = ['pid', 'date', 'gender', 'uid']\n\n\npurchase_data = open_json('data/sub_target/purchase')           \ntarget_purchase = json_normalize(purchase_data, 'products', ['date','uid'])\n\npageview_data = open_json('data/sub_target/products')   \ntarget_pageview = pd.DataFrame(pageview_data, columns=['productId', 'timestamp', 'uid'])\ntarget_pageview.columns = ['pid', 'date', 'uid']")
 
 
-# In[ ]:
+# In[17]:
 
 
-
+catalog.to_csv('catalog_transformed.csv')
+purchase.to_csv('raw_purchase.csv')
+pageview.to_csv('raw_product_pageview_data.csv')
+target_purchase.to_csv('raw_target_purchase.csv')
+target_pageview.to_csv('raw_target_product_pageview.csv')
 
 
 # ## Transform gender and date into numbers
 
-# In[44]:
+# In[18]:
 
 
-get_ipython().run_cell_magic(u'time', u'', u"purchase.gender = list(map(gender_to_bin, purchase.gender.values))\npurchase['hour'] = purchase['date']\npurchase['month'] = purchase['date']\n#purchase['day'] = purchase['date']\n#pageview['hour'] = pageview['date']\n#pageview['month'] = pageview['date']\n#pageview['day'] = pageview['date']\n#purchase.day = list(map(lambda t:convert_date(t).day, purchase.day.values))\npurchase.month = list(map(lambda t:convert_date(t).month, purchase.month.values))\npurchase.hour = list(map(lambda t:convert_date(t).hour, purchase.hour.values))\npurchase.date = list(map(to_weekday, purchase.date.values))\npageview.gender = list(map(gender_to_bin, pageview.gender.values))\n#pageview.day = list(map(lambda t:convert_date(t).day, pageview.day.values))\n#pageview.month = list(map(lambda t:convert_date(t).month, pageview.month.values))\n#pageview.hour = list(map(lambda t:convert_date(t).hour, pageview.hour.values))\npageview.date = list(map(to_weekday, pageview.date.values))\n\ntarget_purchase['hour'] = target_purchase['date']\ntarget_purchase['month'] = target_purchase['date']\n#target_purchase['day'] = target_purchase['date']\n#target_pageview['hour'] = target_pageview['date']\n#target_pageview['month'] = target_pageview['date']\n#target_pageview['day'] = target_pageview['date']\n#target_purchase.day = list(map(lambda t:convert_date(t).day, target_purchase.day.values))\ntarget_purchase.month = list(map(lambda t:convert_date(t).month, target_purchase.month.values))\ntarget_purchase.hour = list(map(lambda t:convert_date(t).hour, target_purchase.hour.values))\ntarget_purchase.date = list(map(to_weekday, target_purchase.date.values))\n#target_pageview.day = list(map(lambda t:convert_date(t).day, target_pageview.day.values))\n#target_pageview.month = list(map(lambda t:convert_date(t).month, target_pageview.month.values))\n#target_pageview.hour = list(map(lambda t:convert_date(t).hour, target_pageview.hour.values))\ntarget_pageview.date = list(map(to_weekday, target_pageview.date.values))")
+get_ipython().run_cell_magic(u'time', u'', u"purchase.gender = list(map(gender_to_bin, purchase.gender.values))\n#purchase['hour'] = purchase['date']\npurchase['month'] = purchase['date']\n#purchase['day'] = purchase['date']\n#pageview['hour'] = pageview['date']\n#pageview['month'] = pageview['date']\n#pageview['day'] = pageview['date']\n#purchase.day = list(map(lambda t:convert_date(t).day, purchase.day.values))\npurchase.month = list(map(lambda t:convert_date(t).month, purchase.month.values))\n#purchase.hour = list(map(lambda t:convert_date(t).hour, purchase.hour.values))\npurchase.date = list(map(to_weekday, purchase.date.values))\npageview.gender = list(map(gender_to_bin, pageview.gender.values))\n#pageview.day = list(map(lambda t:convert_date(t).day, pageview.day.values))\n#pageview.month = list(map(lambda t:convert_date(t).month, pageview.month.values))\n#pageview.hour = list(map(lambda t:convert_date(t).hour, pageview.hour.values))\npageview.date = list(map(to_weekday, pageview.date.values))\n\n#target_purchase['hour'] = target_purchase['date']\ntarget_purchase['month'] = target_purchase['date']\n#target_purchase['day'] = target_purchase['date']\n#target_pageview['hour'] = target_pageview['date']\n#target_pageview['month'] = target_pageview['date']\n#target_pageview['day'] = target_pageview['date']\n#target_purchase.day = list(map(lambda t:convert_date(t).day, target_purchase.day.values))\ntarget_purchase.month = list(map(lambda t:convert_date(t).month, target_purchase.month.values))\n#target_purchase.hour = list(map(lambda t:convert_date(t).hour, target_purchase.hour.values))\ntarget_purchase.date = list(map(to_weekday, target_purchase.date.values))\n#target_pageview.day = list(map(lambda t:convert_date(t).day, target_pageview.day.values))\n#target_pageview.month = list(map(lambda t:convert_date(t).month, target_pageview.month.values))\n#target_pageview.hour = list(map(lambda t:convert_date(t).hour, target_pageview.hour.values))\ntarget_pageview.date = list(map(to_weekday, target_pageview.date.values))")
+
+
+# In[19]:
+
+
+target_pageview.date.values
+
+
+# In[20]:
+
+
+purchase.to_csv('transformed_purchase.csv')
+pageview.to_csv('transformed_product_pageview_data.csv')
+target_purchase.to_csv('transformed_target_purchase.csv')
+target_pageview.to_csv('transformed_target_product_pageview.csv')
 
 
 # ## Set dummies vars for categorical features
 
-# In[45]:
+# In[21]:
 
 
-get_ipython().run_cell_magic(u'time', u'', u"categorical_p = ['category', 'sub_category', 'sub_sub_category', 'date','hour','month']\ncategorical_v = ['category', 'sub_category', 'sub_sub_category', 'date']\n\npurchase = purchase.join(catalog.set_index('pid'), on='pid')\npurchase = pd.get_dummies(purchase, columns=categorical_p).iloc[:,1:]\n\npageview = pageview.join(catalog.set_index('pid'), on='pid')\npageview = pd.get_dummies(pageview, columns=categorical_v).iloc[:,1:]\n\ntarget_purchase = target_purchase.join(catalog.set_index('pid'), on='pid')\ntarget_purchase = pd.get_dummies(target_purchase, columns=categorical_p).iloc[:,1:]\n\ntarget_pageview = target_pageview.join(catalog.set_index('pid'), on='pid')\ntarget_pageview = pd.get_dummies(target_pageview, columns=categorical_v).iloc[:,1:]")
+get_ipython().run_cell_magic(u'time', u'', u"categorical_p = ['category', 'sub_category', 'sub_sub_category', 'date','month']\ncategorical_v = ['category', 'sub_category', 'sub_sub_category', 'date']\n\npurchase = purchase.join(catalog.set_index('pid'), on='pid')\npurchase = pd.get_dummies(purchase, columns=categorical_p).iloc[:,1:]\n\npageview = pageview.join(catalog.set_index('pid'), on='pid')\npageview = pd.get_dummies(pageview, columns=categorical_v).iloc[:,1:]\n\ntarget_purchase = target_purchase.join(catalog.set_index('pid'), on='pid')\ntarget_purchase = pd.get_dummies(target_purchase, columns=categorical_p).iloc[:,1:]\n\ntarget_pageview = target_pageview.join(catalog.set_index('pid'), on='pid')\ntarget_pageview = pd.get_dummies(target_pageview, columns=categorical_v).iloc[:,1:]")
+
+
+# In[22]:
+
+
+purchase.to_csv('dummy_purchase.csv')
+pageview.to_csv('dummy_product_pageview_data.csv')
+target_purchase.to_csv('dummy_target_purchase.csv')
+target_pageview.to_csv('dummy_target_product_pageview.csv')
 
 
 # ## Merge users with same uid (sum entries) and join DFs
 
-# In[46]:
+# In[23]:
 
 
 purchase.current_price = purchase.current_price.values*purchase.quantity
 purchase.original_price = purchase.original_price.values*purchase.quantity
 purchase.diff_price = purchase.diff_price.values*purchase.quantity
 
+target_purchase.current_price = target_purchase.current_price.values*target_purchase.quantity
+target_purchase.original_price = target_purchase.original_price.values*target_purchase.quantity
+target_purchase.diff_price = target_purchase.diff_price.values*target_purchase.quantity
 
-# In[47]:
+
+# In[24]:
 
 
 get_ipython().run_cell_magic(u'time', u'', u"purchase = purchase.groupby(['uid','gender'], as_index=False).sum()\npageview = pageview.groupby(['uid','gender'], as_index=False).sum()\ndata = purchase.join(pageview.set_index(['uid','gender']), on=['uid','gender'], rsuffix='_v')\n\n\ntarget_purchase = target_purchase.groupby(['uid'], as_index=False).sum()\ntarget_pageview = target_pageview.groupby(['uid'], as_index=False).sum()\ntarget_data = target_purchase.join(target_pageview.set_index('uid'), on='uid', rsuffix='_v')")
 
 
-# In[48]:
+# In[25]:
 
 
-purchase.to_csv('grouped_purchase_data.csv')
-pageview.to_csv('grouped_product_pageview_data.csv')
-target_purchase.to_csv('grouped_target_purchase_data.csv')
-target_pageview.to_csv('grouped_target_product_pageview_data.csv')
+purchase.to_csv('grouped_purchase.csv')
+pageview.to_csv('grouped_product_pageview.csv')
+target_purchase.to_csv('grouped_target_purchase.csv')
+target_pageview.to_csv('grouped_target_product_pageview.csv')
+
+
+# In[26]:
+
+
+data.to_csv('final_data.csv')
+target_data.to_csv('final_target_data.csv')
 
 
 # ## Additional features
@@ -132,15 +171,15 @@ target_pageview.to_csv('grouped_target_product_pageview_data.csv')
 # In[97]:
 
 
-search = get_data_pageview('search')
-target_search = get_target_pageview('search')
-data2 = data.join(search.set_index(['uid','gender']), on=['uid','gender'], rsuffix='_s')
-target_data2 = target_data.join(target_search.set_index('uid'), on='uid', rsuffix='_s')
+#search = get_data_pageview('search')
+#target_search = get_target_pageview('search')
+#data2 = data.join(search.set_index(['uid','gender']), on=['uid','gender'], rsuffix='_s')
+#target_data2 = target_data.join(target_search.set_index('uid'), on='uid', rsuffix='_s')
 
-home = get_data_pageview('home')
-target_home = get_target_pageview('home')
-data2 = data2.join(home.set_index(['uid','gender']), on=['uid','gender'], rsuffix='_s')
-target_data2 = target_data2.join(target_home.set_index('uid'), on='uid', rsuffix='_s')
+#home = get_data_pageview('home')
+#target_home = get_target_pageview('home')
+#data2 = data2.join(home.set_index(['uid','gender']), on=['uid','gender'], rsuffix='_s')
+#target_data2 = target_data2.join(target_home.set_index('uid'), on='uid', rsuffix='_s')
 
 
 # In[8]:
@@ -149,7 +188,9 @@ target_data2 = target_data2.join(target_home.set_index('uid'), on='uid', rsuffix
 len(search.uid.values)
 
 
-# In[49]:
+# ## Train and Evaluate Model
+
+# In[27]:
 
 
 data = data.fillna(value=0)
@@ -160,18 +201,30 @@ features2 = target_data.columns[1:]
 features = list(set(features)&set(features2))
 
 
+# In[32]:
+
+
+f = np.array(features)
+
+
+# In[34]:
+
+
+f.tofile('features.csv',sep=',')
+
+
 # In[98]:
 
 
-data2 = data2.fillna(value=0)
-target_data2 = target_data2.fillna(value=0)
-features = data2.columns[2:]
-target = data2.columns[1]
-features2 = target_data2.columns[1:]
-features = list(set(features)&set(features2))
+#data2 = data2.fillna(value=0)
+#target_data2 = target_data2.fillna(value=0)
+#features = data2.columns[2:]
+#target = data2.columns[1]
+#features2 = target_data2.columns[1:]
+#features = list(set(features)&set(features2))
 
 
-# In[94]:
+# In[35]:
 
 
 X = data[features]
@@ -182,18 +235,18 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random
 # In[146]:
 
 
-X = data2[features]
-Y = data2[target]
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.35, random_state=42)
+#X = data2[features]
+#Y = data2[target]
+#x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.35, random_state=42)
 
 
-# In[147]:
+# In[36]:
 
 
 get_ipython().run_cell_magic(u'time', u'', u"forest = RandomForestClassifier(n_estimators=100)\nforest.fit(x_train, y_train)\ny_pred = forest.predict(x_test)\nscore1 = metrics.accuracy_score(y_test, y_pred)\nscore2 = metrics.f1_score(y_test, y_pred, average='binary')\nscore3 = metrics.roc_auc_score(y_test, y_pred)\n#joblib.dump(forest, '83.pkl')\nprint score1, score2, score3")
 
 
-# In[148]:
+# In[37]:
 
 
 from sklearn.ensemble import GradientBoostingClassifier
@@ -208,7 +261,7 @@ print score1, score2, score3
 
 # ## Output
 
-# In[57]:
+# In[38]:
 
 
 X = target_data[features]
